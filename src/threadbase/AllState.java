801dada644abe09eefb31bfe24b10fa4a78ab60f
zhangkaiqiang <13054626536@163.com>
2019-06-11 10:33:06 +0800

    先承认那个状态

---
+++ src/threadbase/AllState.java
@@ -0,0 +1,46 @@
+package threadbase;
+
+import static java.lang.Thread.*;
+
+/**
+ * @Author zhangkaiqiang
+ * @Date 2019/6/10  14:41
+ * @Description   线程的所有状态
+ * Java中对线程状态的定义在与public enum State {}枚举类中
+ */
+public class AllState {
+
+	public static void main(String[] args) {
+
+		Thread thread=new Thread(()->{
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+
+		//NEW
+		State state=thread.getState();
+		System.out.println(state.toString());
+
+		//RUNNABLE
+		thread.start();
+		state=thread.getState();
+		System.out.println(state.toString());
+
+
+		while(state!= State.TERMINATED){
+			//得到当前的线程数
+			int num= activeCount();
+			System.out.println(num);
+			try {
+				sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			state=thread.getState();
+			System.out.println(state.toString());
+		}
+	}
+}
